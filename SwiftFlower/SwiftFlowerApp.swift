f6ebbaf09e2f13501f07402f92ae48f326c7e847
Javier Alaves <jayalaves@gmail.com>
2023-07-18 10:54:49 +0200

    Initial Commit

---
+++ SwiftFlower/SwiftFlowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFlowerApp.swift
+//  SwiftFlower
+//
+//  Created by Javier Alaves on 18/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFlowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
